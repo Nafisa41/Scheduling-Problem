a87070b8d956ffec6b673be337452d4b7d8fd3f3
Nafisa41 <nafisanaznin.cse@gmail.com>
2021-04-11 02:11:08 +0600

    newly edited algo

--- algo.py
+++ algo.py
@@ -1,36 +1,22 @@
-#slot er array ta
-#dictionary for teacher
-#dictionary for timeslot
-#dictionary for year 
-#12 ta theke kono 3 credit class start korte parbo na same goes for 4 ta
+
 import datetime
 import numpy 
 from dataprocess import Teacher
 from dataprocess import Course
 from dataprocess import courselist
 from dataprocess import teacherlist
-from dataprocess import process_class_cnt
+from dataprocess import teacher_course_mp
+year_time = [[],[],[],[],[]] #start time end time day and section
+teacher_time = []
 diction_day = {}
 timeslots = []
 course_cnt = numpy.full((1, 50), 0)
-free = numpy.full((6, 6,80), 0)
-teacher_slot = numpy.full((40, 80), 0)
 valid_time = [
     datetime.datetime.strptime('08:30 AM', '%I:%M %p'),
-    datetime.datetime.strptime('09:00 AM', '%I:%M %p'),
-    datetime.datetime.strptime('09:30 AM', '%I:%M %p'),
     datetime.datetime.strptime('10:00 AM', '%I:%M %p'),
-    datetime.datetime.strptime('10:30 AM', '%I:%M %p'),
-    datetime.datetime.strptime('11:00 AM', '%I:%M %p'),
     datetime.datetime.strptime('11:30 AM', '%I:%M %p'),
-    datetime.datetime.strptime('12:00 PM', '%I:%M %p'),
-    datetime.datetime.strptime('12:30 PM', '%I:%M %p'),
     datetime.datetime.strptime('02:00 PM', '%I:%M %p'),
-    datetime.datetime.strptime('02:30 PM', '%I:%M %p'),
-    datetime.datetime.strptime('03:00 PM', '%I:%M %p'),
     datetime.datetime.strptime('03:30 PM', '%I:%M %p'),
-    datetime.datetime.strptime('04:00 PM', '%I:%M %p'),
-    datetime.datetime.strptime('04:30 PM', '%I:%M %p'),
     ]
 
 def process_diction_day():
@@ -52,10 +38,36 @@ def process_timeslot_list():
     for i in range(5):
         for j in range(len(valid_time)):
             timeslots.append((days[i], valid_time[j]))
-            
-    
-#ektu dekhte hobe abar
-def process_teacher_wanttime(teacher_idx, time_idx):
+
+def process_teacher_time():
+    for i in range(len(teacherlist)):
+        teacher_time[i] = []
+
+def read_course_teacher_free(course_idx, teacher_idx, time_idx, id): #id 0 for course and 1 for teacher
+    flag = True
+    day = timeslots[time_idx][0]
+    start_time = timeslots[time_idx][1]
+    end_time = timeslots[time_idx][1] + courselist[course_idx].duration
+    if(end_time >  datetime.datetime.strptime('05:00 PM', '%I:%M %p') or end_time ==  datetime.datetime.strptime('02:30 PM', '%I:%M %p')):
+        return False
+    list_idx = 0
+    temp_list = []
+    if(id == 0):
+        temp_list = year_time
+        list_idx = courselist[course_idx].course_year
+    else:
+        temp_list = teacher_time
+        list_idx = teacher_idx
+    for i in range(len(temp_list[list_idx])):
+        if(temp_list[list_idx][2] != day):
+            continue
+        st = temp_list[list_idx][0]
+        et = temp_list[list_idx][1]
+        if((start_time >= st and start_time <= et) or (end_time >= st and end_time <= et)):
+            flag = False
+    return flag
+
+def teacher_wanttime(teacher_idx, time_idx):
     day_idx = diction_day[timeslots[time_idx][0]]
     timelist = teacherlist[teacher_idx].valid_time[day_idx]
     i = 0
@@ -64,92 +76,46 @@ def process_teacher_wanttime(teacher_idx, time_idx):
             return True
     return False
 
-def check_time(idx, i): # see if we can finish the class if we start now.
-    if(timeslots[idx][1] == datetime.datetime.strptime('12:30 PM', '%I:%M %p') or timeslots[idx][1] == datetime.datetime.strptime('04:30 PM', '%I:%M %p')):
+def read_course_cnt(course_idx):
+    if(course_cnt[course_idx] >= courselist[course_idx].class_cnt):
         return False
-    if(courselist[i].lab_course == False and courselist[i].credit == 3):
-        if(timeslots[idx][1] == datetime.datetime.strptime('12:00 AM', '%I:%M %p') or timeslots[idx][1] ==  datetime.datetime.strptime('04:00 PM', '%I:%M %p')):
-            return False
-    if(courselist[i].lab_course == True):
-        if( timeslots[idx][1] == datetime.datetime.strptime('08:30 AM', '%I:%M %p') or
-            timeslots[idx][1] == datetime.datetime.strptime('09:00 AM', '%I:%M %p') or
-            timeslots[idx][1] == datetime.datetime.strptime('09:30 AM', '%I:%M %p') or
-            timeslots[idx][1] == datetime.datetime.strptime('10:00 AM', '%I:%M %p') or
-            timeslots[idx][1] == datetime.datetime.strptime('02:00 PM', '%I:%M %p')):
-            return True
-        else:
-            return False
-    return True
-
-
-def update_free(course_idx, year, section, time_idx, keep):
-    day = timeslots[time_idx][0] #sunday or monday
-    crdt = courselist[course_idx].credit
-    loopcnt = 0
-    if(courselist[course_idx].lab_course == True):
-        loopcnt = 6
     else:
-        if(crdt == 2) loopcnt = 2
-        else loopcnt = 3 
-    i = time_idx
-    val = time_idx + loopcnt
-    for i in range(val)):
-        free[year][section][i] = keep
-
-def update_teacherslot(teacher_idx, time_idx, keep):
-    day = timeslots[time_idx][0] #sunday or monday
-    crdt = courselist[course_idx].credit
-    loopcnt = 0
-    if(courselist[course_idx].lab_course == True):
-        loopcnt = 6
-    else:
-        if(crdt == 2) loopcnt = 2
-        else loopcnt = 3 
-    i = time_idx
-    val = time_idx + loopcnt
-    for i in range(val)):
-        free[teacher_idx][i] = keep
-
-
-process_timeslot_list()
-process_diction_day()
-
-def main_algo(idx):
-    #base case
-    if(idx == 75):
-        return
-    i = 0
-    for i in range(len(courselist)):
-        #pichoner slot gulao dekhte hobe
-        str = courselist[i].course_name.split(' ')
-        year = int(str[1][0])
-        section = 0
-        if(courselist[i].lab_course == True):
-            section = int(str[3][0])
-        tot_class = course_cnt[i]
-        if(free[year][section][idx] == 0 and tot_class < courselist[i].class_cnt and check_time(idx, i) == True):
-            for j in range(len(teacherlist)):
-                want_course = False
-                want_time = False
-                free_time = False
-                if(courselist[i].course_name in teacherlist[j].assigned_courses):
-                    want_course = True
-                if(process_teacher_wanttime(j,idx)):
-                    want_time = True
-                if(teacher_slot[j][idx] == 0):
-                    free_time = True
-                if(want_course == True and want_time == True and free_time == True):
-                    course_cnt[i] += 1
-                    update_free(i, year, section, idx, 1)
-                    update_teacherslot(j, idx, 1)
-                    main_algo(idx + 1)
-                    course_cnt[i] -= 1
-                    update_free(i, year, section, idx, 0)
-                    update_teacherslot(j, idx, 0)
-
-                
-
-
-
-
+        return True
     
+def find_teacher_idx(teacher_name):
+    for i in (len(teacherlist)):
+        if(teacherlist[i].teacher_initial == teacher_name):
+            return i
+
+def update_course_free(course_idx, time_idx):
+    year = courselist[course_idx].course_year
+    templist = []
+    templist[0] = timeslots[time_idx][1]
+    templist[1] = timeslots[time_idx][1] + courselist[course_idx].duration
+    templist[2] = timeslots[time_idx][0]
+    templist[3] = courselist[course_idx].course_name
+    year_time[year].append(templist)
+
+def update_teacher_free(teacher_idx, time_idx, course_idx):
+    templist = []
+    templist[0] = timeslots[time_idx][1]
+    templist[1] = timeslots[time_idx][1] + courselist[course_idx].duration
+    templist[2] = timeslots[time_idx][0]
+    templist[3] = courselist[course_idx].course_name
+    teacher_time[teacher_idx].append(templist)
+
+def udpate_course_cnt(course_idx, val):
+    course_cnt[idx] = course_cnt[idx] + val
+
+def func(idx):
+    for i in range(len(courselist)):
+        teacher_idx = find_teacher_idx(teacher_course_mp[courselist[i].course_name])
+        year_free = read_course_teacher_free(i, teacher_idx, idx, 0)
+        teacher_free = read_course_teacher_free(i, teacher_idx, idx, 1)
+        if(year_free == True and teacher_free == True and course_cnt[i] == True):
+            func(idx + 1)
+        
+
+
+process_diction_day()
+process_timeslot_list()
--- dataprocess.py
+++ dataprocess.py
@@ -1,3 +1,7 @@
+#things to remember:
+# you took credit as float
+#lab class ekta kore dhorsi always
+#shob section ke alada course hishabe chinta korsi
 import pandas as pd 
 import numpy as np
 import math
@@ -5,46 +9,69 @@ import datetime
 courselist = []
 teacherlist = []
 sectionthree = []
+dict_credit = {}
+teacher_course_mp = {}
 class Course:
-    def __init__(self, course_name, credit, section_count, course_year, lab_course, class_cnt):
+    def __init__(self, course_name, credit, course_year, lab_course, class_cnt):
         self.course_name = course_name
         self.credit = credit
-        self.section_count = section_count
         self.course_year = course_year    
         self.lab_course = lab_course 
         self.class_cnt = class_cnt
+        if(self.lab_course == True):
+            self.duration = datetime.timedelta(hours=3)
+        elif(self.credit == 3):
+            self.duration = datetime.timedelta(hours=1.5)
+        elif(self.credit == 2):
+            self.duration = datetime.timedelta(hours=1)
 
         
 class Teacher:
     def __init__(self, teacher_initial, assigned_courses, valid_time):
         self.teacher_initial = teacher_initial
         self.assigned_courses = assigned_courses
-        self.valid_time = valid_time
+        self.valid_time = valid_time   
 
 
-##functions
-def process_class_cnt(credit):
-    if(credit == 3):
-        return 2
-    elif(credit == 2):
-        return 2
-    elif(credit == 1.5):
+def process_curriculum():
+    df_curr = pd.read_excel("ASST 03_ Input.xlsx", "UndergradCurriculum (Pre-fed)")
+    curr_matrix = df_curr.to_numpy()
+    i = 0
+    for i in range((curr_matrix.shape[0])):
+        dict_credit[curr_matrix[i][1]] = float(curr_matrix[i][2])
+
+def process_classcnt(credit, is_lab):
+    if(is_lab == True):
         return 1
-    elif(credit == .75):
-        return 1
+    if(credits == 3.0):
+        return 2
+    if(credits == 2):
+        return 2
 
 def process_course():
-
     #reading data from undergradcurriculum for courses
-    df_course = pd.read_excel("ASST 03_ Input.xlsx", "UndergradCurriculum (Pre-fed)")
+    df_course = pd.read_excel("ASST 03_ Input.xlsx", "AssignedCourses")
     course_matrix = df_course.to_numpy()
-    i = 0
+    course_matrix[0][0]
     for i in range(course_matrix.shape[0]):
-        chunks = course_matrix[i][1].split(' ')
-        if(chunks[1][2] == '1'):
-            courselist.append(Course(course_matrix[i][1], course_matrix[i][2], 2, chunks[1][0], chunks[1][2] == '1', process_class_cnt(float(course_matrix[i][2]))))
-        else:
-            courselist.append(Course(course_matrix[i][1], course_matrix[i][2], 0, chunks[1][0], chunks[1][2] == '1', process_class_cnt(float(course_matrix[i][2]))))
+        for j in range(course_matrix.shape[1]):
+            if(pd.isnull(course_matrix[i][j]) or j == 0):
+                continue
+            chunks = course_matrix[i][j].split(' ')
+            crs_chk = chunks[0] + ' ' + chunks[1]
+            if(crs_chk in dict_credit):
+                crs_credit = dict_credit[crs_chk]
+            else: 
+                continue
+            crs_year = int(chunks[1][0])
+            crs_lab = (chunks[1][2] == '1')
+            crs_classcnt = process_classcnt(crs_credit, crs_lab)
+            teacher_course_mp[course_matrix[i][j]] = course_matrix[i][0]
+            courselist.append(Course(course_matrix[i][j], crs_credit, crs_year, crs_lab, crs_classcnt))
+
+    # i = 0
+    # for i in range(len(courselist)):
+    #     print(courselist[i].course_name + ' ' + str(courselist[i].credit) + ' ' + str(courselist[i].course_year) + ' ' + str(courselist[i].lab_course) + ' ' + str(courselist[i].class_cnt))
 
 
 def process_teacher():
@@ -57,7 +84,6 @@ def process_teacher():
     j = 0
     for i in range(assignedcrs_matrix.shape[0]):
         crslist = []
-        timeslt = []
         for j in range(assignedcrs_matrix.shape[1]):
             if(j == 0):
                 continue
@@ -67,18 +93,15 @@ def process_teacher():
                 if(len(section_str) == 4 and int(section_str[3][0]) == 3):
                     sectionthree.append(section_str[0] + ' ' + section_str[1])
                     
-
+        temp_list = []
         for j in range(validtime_matrix.shape[1]):
-            temp_list = []
             if(j == 0):
                 continue
             if(pd.isnull(validtime_matrix[i][j]) == False):
                 temp_list.append(time_convert(validtime_matrix[i][j]))
             else:
-                temp_list.append('Not available')
-            timeslt.append(temp_list)
-
-        teacherlist.append(Teacher(assignedcrs_matrix[i][0], crslist, timeslt))        
+                temp_list.append([])
+        teacherlist.append(Teacher(assignedcrs_matrix[i][0], crslist, temp_list))        
                 
     
 def time_convert(str):
@@ -89,31 +112,11 @@ def time_convert(str):
         startTime = datetime.datetime.strptime(startTime, '%I:%M%p')
         endTime = t.split('-')[1]
         endTime = datetime.datetime.strptime(endTime, '%I:%M%p')
-        timelist.append((startTime, endTime))
+        timelist.append([startTime, endTime])
     return timelist
 
 
-def process_section():
-    i = 0
-    for i in range(len(sectionthree)):
-        idx = [x.course_name for x in courselist].index(sectionthree[i])
-        courselist[idx].section_count = 3
-
-def process_final_courselist():
-    i = 0
-    for i in range(len(courselist)):
-        if(courselist[i].section_count == 2 and len(courselist[i].course_name.split(' ')) != 4):
-            temp = courselist[i]
-            courselist.remove(courselist[i])
-            temp.course_name  = temp.course_name + ' ' + 'Section' + ' ' + '2'
-            courselist.append(temp)
-        elif(courselist[i].section_count == 3 and len(courselist[i].course_name.split(' ')) != 4):
-            temp = courselist[i]
-            courselist.remove(courselist[i])
-            temp.course_name  = temp.course_name + ' ' + 'Section' + ' ' + '3'
-            courselist.append(temp)
-
+process_curriculum()
 process_course()
 process_teacher()
-process_section()
-process_final_courselist()
+print(teacher_course_mp)
